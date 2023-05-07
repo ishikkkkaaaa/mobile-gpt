82392e0464f265e1a5a310450116f6e74b2a1aa0
imusun24@gmail.com <imusun24@gmail.com>
2023-05-07 13:27:22 +0530

    adding functionalities to buttons

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# route for handling control commands
+@app.route('/rotate')
+def rotate():
+    cmd = request.args.get('cmd')
+    if cmd == 'left':
+        # Rotate left code here
+        pass
+    elif cmd == 'right':
+        # Rotate right code here
+        pass
+    else:
+        return 'Invalid command'
 
 if __name__ == '__main__':
     app.run(debug=False)
